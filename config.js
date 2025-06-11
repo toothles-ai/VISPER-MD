755ca6ae2ec0656f6b5b0dae3e568bdae7a2692f
toothles-ai <tharushanamadara0000@gmail.com>
2025-06-11 18:30:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'PUT SESSION ID HERE' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'MOVIE-VISPER=Lo4gUIbT#UzNKe3uik9Fd_f4SJNPF0Bjv_1EKrVhfvViN1qoGnV4' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME, 
 
